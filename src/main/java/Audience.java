09b2a9b50d10f1ff4457f05742b1deb03edba23d
phantom08266 <phantom08266@gmail.com>
2022-10-07 18:30:57 +0900

    Feat 소극장 연극 예매 구현

---
+++ src/main/java/Audience.java
@@ -0,0 +1,10 @@
+import lombok.Getter;
+
+@Getter
+public class Audience {
+    private Bag bag;
+
+    public Audience(Bag bag) {
+        this.bag = bag;
+    }
+}
---
+++ src/main/java/TicketSeller.java
@@ -0,0 +1,10 @@
+import lombok.Getter;
+
+@Getter
+public class TicketSeller {
+    private TicketOffice ticketOffice;
+
+    public TicketSeller(TicketOffice ticketOffice) {
+        this.ticketOffice = ticketOffice;
+    }
+}
